927e98c40da58b848b9f34f63798600c473cd11d
maikalangi <maikalangi@gmail.com>
2022-05-17 20:26:08 -0700

    sets up show route

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/drinks/', (req, res)=>{
     res.render('drinks_index.ejs', { drinks });
 });
 
+app.get('/drinks/:id', (req, res)=>{
+    res.send(req.params.id);
+});
+
 app.listen(port, ()=>{
     console.log('listening to port ', port);
 });
