ad2a93943f36444c8e0b14fa6269b42569ce6bee
maikalangi <maikalangi@gmail.com>
2022-05-17 22:43:15 -0700

    adds food database, index, and show pages

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const res = require('express/lib/response');
 const drinks = require('./models/drinks');
+const food = require('./models/food');
 
 const app = express();
 
 const port = 3000;
 
 app.get('/', (req, res)=>{
-    res.send('Welcome to the Gitpub App');
+    res.render('root.ejs');
 });
 
 app.get('/drinks/', (req, res)=>{
     res.render('drinks_index.ejs', { drinks });
 });
 
+app.get('/food/', (req, res)=>{
+    res.render('food_index.ejs', { food });
+});
+
 app.get('/drinks/:id', (req, res)=>{
     res.render('drinks_show.ejs', {
         drinks: drinks[req.params.id]
+    });
+});
+
+app.get('/food/:id', (req, res)=>{
+    res.render('food_show.ejs', {
+        food: food[req.params.id]
     });
 });
 
